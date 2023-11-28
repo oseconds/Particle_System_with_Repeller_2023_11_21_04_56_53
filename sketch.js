ec99313ec9c520a79757b80f2684edff9360ecf6
oseconds <123317581+oseconds@users.noreply.github.com>
2023-11-28 17:00:10 +0900

    성공
    createGui 대신  QuickSettings.create 로 만들면 destroy 펑션을 사용할 수 있다

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ let pSlider;
 
 var gui;
 
-var guis = []; 
+var guis = [];
 
 // New variables for GUI
 var strength = 10;
@@ -25,25 +25,25 @@ function setup() {
   createCanvas(innerWidth, innerHeight);
   emitter = new Emitter(width / 2, height / 2);
   repeller = new Repeller(width / 2, 500, strength);
-  att = new Attractor(width/2, height / 2);
+  att = new Attractor(width / 2, height / 2);
 
   // pSlider = createSlider(-100, 100, 10);
   // let sliderWidth = Math.min(width/2, 300);
   // pSlider.style('width', sliderWidth + 'px');
   // // pSlider.style('width', width/2 + 'px');
   // pSlider.position(width/2 - pSlider.width/2, height - 130);
-  
-  gui = createGui('My GUI');
-  gui.addGlobals('strength');
+
+  gui = QuickSettings.create(10, 10, 'My GUI');
+  gui.addRange('strength', strengthMin, strengthMax, initialStrength, 1);
 
   var resetButton = createButton('Reset');
   resetButton.position(20, 120);
   resetButton.mouseClicked(resetValues);
   resetButton.style('z-index', '1');
-  
+
 
   // noLoop();
-  
+
 }
 
 function draw() {
@@ -53,14 +53,14 @@ function draw() {
   //let strength = pSlider.value();
   repeller.setStrength(strength);
 
-  
+
   let gravity = createVector(0, 0.1);
   emitter.applyForce(gravity);
-  
+
   emitter.applyAttractor(att);
   emitter.applyRepeller(repeller);
   repeller.position = createVector(mouseX, mouseY);
-  
+
   att.update();
 
   repeller.show();
@@ -70,11 +70,11 @@ function draw() {
   if (keyIsDown(UP_ARROW)) {
     strength += 5;
   }
-  
+
   if (keyIsDown(DOWN_ARROW)) {
     strength -= 5;
   }
-  
+
   if (keyIsDown(32)) { // 'SPACE' key
     strength = 10;
   }
@@ -85,28 +85,14 @@ function draw() {
 
 }
 
-function destroyGui(gui) {
-  // Find the index of the GUI in the guis array
-  var index = guis.indexOf(gui);
-
-  // If the GUI was found, remove it from the array
-  if (index !== -1) {
-      guis.splice(index, 1);
-  }
-
-  // Call the destroy method of the gui object
-  if (gui && typeof gui.destroy === 'function') {
-      gui.destroy();
-  }
-}
-
 
 function resetValues() {
   strength = initialStrength;
+  x = gui.x;
+  y = gui.y;
 
-  destroyGui(gui); // Call the destroyGui function
+  destroyGui(gui);
 
-  gui = createGui('My GUI'); // Assign a new GUI to the gui variable
-  gui.addGlobals('strength');
-  guis.push(gui); // Add the new GUI to the guis array
+  gui = QuickSettings.create(x, y, 'My GUI');
+  gui.addRange('strength', strengthMin, strengthMax, initialStrength, 1);
 }
